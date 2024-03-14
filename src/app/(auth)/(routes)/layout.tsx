50df58b5c7a67b1e7192f1d866cd377f410ab941
Deren Somsanith <derensomsanith@gmail.com>
2024-03-14 12:13:10 -0500

    - show welcome screen for now - center the sign up a bit

--- src/app/(auth)/(routes)/layout.tsx
+++ src/app/(auth)/(routes)/layout.tsx
@@ -4,7 +4,9 @@ type AuthLayoutProps = {
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
   return (
-    <div className="flex h-full items-center justify-center">{children}</div>
+    <div className="flex h-full items-center justify-center py-24">
+      {children}
+    </div>
   );
 };
 
